675df078e02476ac60b88831bb422c6ddf0c6e0c
maneater2 <114833619+maneater2@users.noreply.github.com>
2024-06-18 18:39:50 +0000

    Update default.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,25 +1,25 @@
 {inputs, username, host, ...}: {
   imports =
        [(import ./aseprite/aseprite.nix)]         # pixel art editor
-    ++ [(import ./audacious/audacious.nix)]       # music player
-    ++ [(import ./bat.nix)]                       # better cat command
-    ++ [(import ./btop.nix)]                      # resouces monitor 
-    ++ [(import ./cava.nix)]                      # audio visualizer
-    ++ [(import ./discord.nix)]                   # discord with catppuccin theme
-    ++ [(import ./gaming.nix)]                    # packages related to gaming
-    ++ [(import ./git.nix)]                       # version control
-    ++ [(import ./gtk.nix)]                       # gtk theme
-    ++ [(import ./hyprland)]                      # window manager
-    ++ [(import ./kitty.nix)]                     # terminal
-    ++ [(import ./mako.nix)]                      # notification deamon
-    ++ [(import ./nvim.nix)]                      # neovim editor
-    ++ [(import ./packages.nix)]                  # other packages
-    ++ [(import ./scripts/scripts.nix)]           # personal scripts
-    ++ [(import ./starship.nix)]                  # shell prompt
-    ++ [(import ./swaylock.nix)]                  # lock screen
-    ++ [(import ./micro.nix)]                     # nano replacement
-    ++ [(import ./vscodium.nix)]                  # vscode fork
-    ++ [(import ./waybar)]                        # status bar
-    ++ [(import ./wofi.nix)]                      # launcher
-    ++ [(import ./zsh.nix)];                      # shell
+    ++ [(import ./audacious/audacious.nix)]  # music player
+    ++ [(import ./bat.nix)]                                     # better cat command
+    ++ [(import ./btop.nix)]                                  # resouces monitor 
+    ++ [(import ./cava.nix)]                                  # audio visualizer
+    ++ [(import ./discord.nix)]                           # discord with catppuccin theme
+    ++ [(import ./gaming.nix)]                             # packages related to gaming
+    ++ [(import ./git.nix)]                                   # version control
+    ++ [(import ./gtk.nix)]                                   # gtk theme
+    ++ [(import ./hyprland)]                                 # window manager
+    ++ [(import ./kitty.nix)]                               # terminal
+    ++ [(import ./mako.nix)]                                 # notification deamon
+    ++ [(import ./nvim.nix)]                                 # neovim editor
+    ++ [(import ./packages.nix)]                        # other packages
+    ++ [(import ./scripts/scripts.nix)]         # personal scripts
+    ++ [(import ./starship.nix)]                        # shell prompt
+    ++ [(import ./swaylock.nix)]                        # lock screen
+    ++ [(import ./micro.nix)]                              # nano replacement
+    ++ [(import ./vscodium.nix)]                       # vscode fork
+    ++ [(import ./waybar)]                                    # status bar
+    ++ [(import ./wofi.nix)]                               # launcher
+    ++ [(import ./zsh.nix)];                               # shell
 }
