ac88ee52755b82695af3970a5c3f0b7cf26834c9
maneater2 <114833619+maneater2@users.noreply.github.com>
2024-06-18 18:32:49 +0000

    Update xserver.nix

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -1,20 +1,21 @@
 { pkgs, username, ... }: 
 {
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us";
-
-    displayManager.autoLogin = {
+  services.displayManager.autoLogin = {
       enable = true;
       user = "${username}";
     };
-    libinput = {
+
+  libinput = {
       enable = true;
       # mouse = {
       #   accelProfile = "flat";
       # };
     };
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
   };
+
   # To prevent getting stuck at shutdown
   systemd.extraConfig = "DefaultTimeoutStopSec=10s";
 }
